1a1127a4baf412da1a57a449b8a70d38287a186f
Ellen Higgins ATU <114396639+g00413157@users.noreply.github.com>
2024-11-11 15:08:09 +0000

    4

--- src/ie/atu/streamlab/excersiseFour.java
+++ src/ie/atu/streamlab/excersiseFour.java
@@ -1,5 +1,20 @@
 package ie.atu.streamlab;
 
-public class excersiseFour {
+import java.util.Arrays;
+import java.util.List;
 
+public class excersiseFour {
+ public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David");
+
+        // Using lambda
+        names.stream()
+             .map(name -> name.toUpperCase())
+             .forEach(name -> System.out.println(name));
+
+        // Using method references
+        names.stream()
+             .map(String::toUpperCase)
+             .forEach(System.out::println);
+    }
 }
